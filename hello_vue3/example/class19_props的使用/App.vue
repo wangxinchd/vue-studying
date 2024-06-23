5af7f7f8327e92e15fbf11f03fcd55712d99f2b2
wangxin91 <wangxin91@xiaomi.com>
2024-06-23 16:12:34 +0800

    add hello_vue3/example/class19_props的使用/

---
+++ hello_vue3/example/class19_props的使用/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="app">
+        <!--  传递props给子组件 -->
+        <Person a="haha" :list="persons" /> <!-- :list传递数组对象不是字符串 -->
+        <br>
+        <p>下面不传递参数</p>
+        <Person a="haha"  /> <!-- :list传递数组对象不是字符串 -->
+    </div>
+</template>
+
+<script lang="ts" setup name="App">
+import { reactive } from 'vue';
+import Person from './components/Person.vue';
+import { type Persons } from '@/types';
+
+let persons = reactive<Persons>([
+    { id: '1', name: '张三', age: 25 },
+    { id: '2', name: '王五', age: 28 }
+]);
+
+
+</script>
+
+<style scoped>
+.app {
+    background-color: #ddd;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+</style>
---
+++ hello_vue3/example/class19_props的使用/components/Person.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="person">
+        <ul>
+            <!-- :key 后面的数据不是字符串，而是对应的变量 -->
+            <li v-for="(item, index) in list" :key="index">{{ item }}</li>
+        </ul>
+    </div>
+
+</template>
+
+<!-- 选项式 app -->
+<script lang="ts" setup name="Person">
+import type { Persons } from '@/types';
+import { defineProps, withDefaults } from 'vue';
+
+// 接收父组件传递的 props
+// let x = defineProps(['a', 'list']);
+
+// 限制父组件传递类型
+// let x = defineProps<{list: Persons}>()
+// console.log("props: ", x)
+
+
+// 接收list + 限制类型 + 限制必要性 + 执行默认类型
+let x = withDefaults(defineProps<{ list?: Persons }>(),
+    {
+        list: () => [   // 这里需要写成一个函数
+            {
+                id: '1',
+                name: '张三2333',
+                age: 20
+            },
+            {
+                id: '2',
+                name: '李四4555',
+                age: 25,
+            }
+        ]
+    }) // ?: 可选参数
+
+</script>
+
+<style scoped>
+.person {
+    background-color: #3cce17;
+    border: 1px solid #ccc;
+}
+
+li {
+    font-size: 20px;
+}
+</style>
